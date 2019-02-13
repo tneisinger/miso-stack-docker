6eae3a1ca1f87f845a4847e0be0075380dbdcc61
tjneisi <tjneisi@gmail.com>
2019-02-13 14:36:12 -0800

    Bug fix in stack-build-docker.sh

--- stack-build-docker.sh
+++ stack-build-docker.sh
@@ -5,13 +5,13 @@ DOCKER_IMAGE="ghcjs:lts-9.21"
 if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
   echo "λ Didn't find a docker image name $DOCKER_IMAGE, building one..."
   docker build -t $DOCKER_IMAGE .
-  echo "λ Copying over the stack root folder to .stack-docker in the project root"
-  docker run -v $(pwd):/src -it $DOCKER_IMAGE cp -R /root/.stack /src/.stack-docker
 fi
 
 
 mkdir -p result/bin \
   && mkdir -p result/static \
+  && echo "λ Copying over the stack root folder to .stack-docker in the project root" \
+  && docker run -v $(pwd):/src -it $DOCKER_IMAGE cp -R /root/.stack /src/.stack-docker \
   && echo "λ Building the frontend..." \
   && docker run -v $(pwd):/src -it $DOCKER_IMAGE stack --stack-root /src/.stack-docker --stack-yaml=frontend/stack.yaml build \
   && echo "λ Copying over all.js" \
